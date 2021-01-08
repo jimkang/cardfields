af8c2f7a8c810f859988c6c3ba095e4392d74538
Jim Kang <jimkang@gmail.com>
2021-01-07 20:25:56 -0500

    Drop unneeded crud.

--- vats/stores/Stores-Test-Container.svelte
+++ vats/stores/Stores-Test-Container.svelte
@@ -8,9 +8,9 @@ var card: Card = {
   id: 'unique-card',
   text: 'Clean inbox all the way!',
   secretText: 'Delete incriminating evidence!',
-  //picture: 'https://smidgeo.com/images/smidgeo_headshot.jpg',
-  picture:
-    'https://smidgeo.com/notes/deathmtn/media/54F917E8-D61B-4D7D-8537-7073D2E53713.jpeg',
+  picture: 'https://smidgeo.com/images/smidgeo_headshot.jpg',
+  //picture:
+  //  'https://smidgeo.com/notes/deathmtn/media/54F917E8-D61B-4D7D-8537-7073D2E53713.jpeg',
   tags: ['email', 'maintenance', 'chore'],
   color: 'hsl(0, 50%, 50%)'
 };
