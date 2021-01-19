d5a66b6decc11e59ad04242d6d467e8a8f73d311
Jim Kang <jimkang@gmail.com>
2021-01-19 10:58:16 -0500

    Add piles test script.

--- vats/groups/GroupsVatContainer.svelte
+++ vats/groups/GroupsVatContainer.svelte
@@ -37,8 +37,8 @@ function onProfileChange() {
 
 onProfileChange();
 </script>
-
-<main>
+<div class="outer" style="display: flex">
+<main style="flex-grow: 1; width: 30em">
   <h1>Groups vat</h1>
 
   <h2>Profile</h2>
@@ -66,10 +66,46 @@ onProfileChange();
   <ExportComp allCardsStore={allCardsStore} />
   <ImportComp allCardsStore={allCardsStore} cardStoreIssuer={cardStoreIssuer} state={state} />
 
-  <div>
+  </main>
+
+  <div class="test-script" style="max-width: 20rem">
     Piles test script:
     <dl>
-      <dt>TODO</dt>
+      <dt>Delete all piles</dt>
+      <dt>Create a new pile named "Discard".</dt>
+      <dt>Reload.</dt>
+      <dd>It should still exist.</dd>
+      <dt>Create another pile named "Draw".</dt>
+      <dt>Add three cards named "Discard A", "Discard B", and "Discard C" to the Discard pile.</dt>
+      <dt>Reload.</dt>
+      <dd>They should still be in that pile and also in the all cards list.</dd>
+      <dt>Add two cards named "Draw A" and "Draw B" to the Draw pile.</dt>
+      <dt>Reload.</dt>
+      <dd>They should still be in that pile and also in the all cards list. The Discard pile should be in the same pile as before.</dd>
+      <dt>Set the description Discard B to "Giza Power Plant."</dt>
+      <dt>Set the description Draw B to "Hovering Lifeless."</dt>
+      <dd>The descriptions should also change in the all cards list.</dd>
+      <dt>Reload.</dt>
+      <dd>Everything should be the same.</dd>
+      <dt>Create another pile named "The Void".</dt>
+      <dt>Remove Discard A from the Discard pile.</dt>
+      <dd>Discard A should not be in the Discard pile, but it should be in the all cards list.</dd>
+      <dt>Reload.</dt>
+      <dd>Everything should be the same.</dd>
+      <dt>Move Discard B to The Void pile.</dt>
+      <dt>Delete the Discard pile.</dt>
+      <dd>Discard C should still be in the All Cards list, even if it isn't in a pile.</dd>
+      <dt>Delete Draw A from the All Cards list.</dt>
+      <dd>It should be gone from the Draw pile.</dd>
+      <dt>Reload.</dt>
+      <dd>Everything should be the same.</dd>
+      <dt>Move Draw A to The Void.</dt>
+      <dt>Move Discard B to Draw.</dt>
+      <dt>Add a cards named "Draw D" to the Draw pile.</dt>
+      <dd>The Void should contain Draw A.</dd>
+      <dd>Draw should Discard B and D.</dd>
+      <dt>Reload.</dt>
+      <dd>Everything should be the same.</dd>
     </dl>
-  </div>
-</main>
+    </div>
+</div>
