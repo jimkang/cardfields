79734f56355fd161d39d5c38d928e57d1e8d3412
Jim Kang <jimkang@gmail.com>
2021-03-03 19:13:57 -0500

    Test unsubscribe.

--- wily.js/tests/store-tests.js
+++ wily.js/tests/store-tests.js
@@ -3,62 +3,52 @@ var test = require('tape');
 var { ThingStore } = require('./build/wily.js/stores.js');
 var { MemoryPersister } = require('./fixtures/memory-persister');
 
+var store = ThingStore(MemoryPersister(), 5);
+
+var cbCallCounts = [0, 0];
+var cbExpected = [[6], [6, 7]];
+var currentTest;
+
+test('Get', testGet);
 test('Store subscriptions', testSubscriptions);
+test('Unsubscribe', testUnsubscribe);
+
+function testGet(t) {
+  t.equal(store.get(), 5, 'get works.');
+  t.end();
+}
 
 function testSubscriptions(t) {
-  t.plan(3);
-
-  var store = ThingStore(MemoryPersister(), 5);
-
-  var cbCallCounts = [0, 0];
-  var cbExpected = [[6, 6], [6]];
-
-  t.equal(store.get(), 5, 'get works.');
+  t.plan(2);
+  currentTest = t;
 
   store.subscribe(cb1);
   store.subscribe(cb2);
   store.set(6);
+}
 
-  function cb1(aStore) {
-    t.equal(
-      aStore.get(),
-      cbExpected[cbCallCounts[0]][0],
-      'Subscriber 1 gets updated value.'
-    );
-  }
-  function cb2(aStore) {
-    t.equal(
-      aStore.get(),
-      cbExpected[cbCallCounts[1]][0],
-      'Subscriber 2 gets updated value.'
-    );
-  }
-  /*
-    function checkResult(error, value) {
-      if (testCase.errorMsg) {
-        t.equal(error.message, testCase.errorMsg, 'Error message is correct.');
-        if (testCase.errorInfo) {
-          console.log('error check', error.message);
-          t.deepEqual(
-            VError.info(error),
-            testCase.errorInfo,
-            'Error info is correct.'
-          );
-        }
-      } else {
-        console.log('Error:', error);
-        assertNoError(t.ok, error, 'No error as expected.');
-        if (testCase.value) {
-          if (Array.isArray(testCase.value)) {
-            testCase.value.forEach((expectedItem, i) =>
-              t.deepEqual(value[i], expectedItem, 'Value element is correct.')
-            );
-          } else {
-            t.deepEqual(value, testCase.value, 'Value is correct.');
-          }
-        }
-      }
-      t.end();
-    }
-*/
+function testUnsubscribe(t) {
+  t.plan(1);
+  currentTest = t;
+
+  store.unsubscribe(cb1);
+  store.set(7);
+}
+
+function cb1(aStore) {
+  currentTest.equal(
+    aStore.get(),
+    cbExpected[0][cbCallCounts[0]],
+    'Subscriber 1 gets updated value.'
+  );
+  cbCallCounts[0] += 1;
+}
+
+function cb2(aStore) {
+  currentTest.equal(
+    aStore.get(),
+    cbExpected[1][cbCallCounts[1]],
+    'Subscriber 2 gets updated value.'
+  );
+  cbCallCounts[1] += 1;
 }
