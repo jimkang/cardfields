6acb0bfb4c8f88df2a65b25dc8773739e9720729
Jim Kang <jimkang@gmail.com>
2021-03-07 18:10:43 -0500

    Format after updating to 2.2.1, which understands 'import type'.

--- vats/profiles/profiles-vat.ts
+++ vats/profiles/profiles-vat.ts
@@ -1,31 +1,60 @@
 //import type { Card } from '../../types';
 import { ThingStore, CollectionStore } from '../../wily.js/stores';
 import type { Profile, ThingStoreType } from '../../types';
-import { thingPersister, IdsPersister, loadThings} from '../../wily.js/persistence/local';
+import {
+  thingPersister,
+  IdsPersister,
+  loadThings,
+} from '../../wily.js/persistence/local';
 import { v4 as uuid } from 'uuid';
 import { UpdateCollection } from '../../wily.js/updaters/basic-updaters';
 import { UpdateProfile } from '../../updaters/updaters';
 import { AddThing } from '../../wily.js/downdaters/collection-modifiers';
-import { RenderProfile, RenderProfileCollection } from '../../renderers/profile-renderers';
+import {
+  RenderProfile,
+  RenderProfileCollection,
+} from '../../renderers/profile-renderers';
 
 var container = {};
 const idsKey = 'ids__profiles';
 var idsPersister = IdsPersister(idsKey);
 
-var collectionStore = CollectionStore(idsPersister, thingPersister, loadThings(idsKey));
-var profileStores = collectionStore.get().map(thing => ThingStore(thingPersister, thing));
-var activeProfileStore = ThingStore(thingPersister, { id: 'active-profile', profile: '' });
-var addThing = AddThing(collectionStore, createNewProfile, thingPersister, createItemUpdater);
+var collectionStore = CollectionStore(
+  idsPersister,
+  thingPersister,
+  loadThings(idsKey)
+);
+var profileStores = collectionStore
+  .get()
+  .map((thing) => ThingStore(thingPersister, thing));
+var activeProfileStore = ThingStore(thingPersister, {
+  id: 'active-profile',
+  profile: '',
+});
+var addThing = AddThing(
+  collectionStore,
+  createNewProfile,
+  thingPersister,
+  createItemUpdater
+);
 
-var renderCollection = RenderProfileCollection({ parentSelector: '.root', addThing });
+var renderCollection = RenderProfileCollection({
+  parentSelector: '.root',
+  addThing,
+});
 
 var updateCollection = UpdateCollection(renderCollection, collectionStore);
 updateCollection(collectionStore);
 var updateItems = profileStores.map(createItemUpdater);
-updateItems.forEach((update, i) => update(profileStores[i])); 
+updateItems.forEach((update, i) => update(profileStores[i]));
 
 function createItemUpdater(store: ThingStoreType) {
-  return UpdateProfile(RenderProfile({ parentSelector: `#${store.get().id}`  }), collectionStore, activeProfileStore, store);
+  return UpdateProfile(
+    RenderProfile({ parentSelector: `#${store.get().id}` }),
+    collectionStore,
+    activeProfileStore,
+    store
+  );
 }
 
 function createNewProfile(): Profile {
