d64ac8b26dc6e1bf2351fb28d6e4140e45885ebc
Jim Kang <jimkang@gmail.com>
2021-04-30 11:00:41 -0400

    Strip out old Svelte stuff.

--- vats/groups/groups-vat-app.ts
+++
@@ -1,7 +0,0 @@
-import App from './GroupsVatContainer.svelte';
-
-const app = new App({
-  target: document.body
-});
-
-export default app;
--- vats/stores/stores-test-app.ts
+++
@@ -1,7 +0,0 @@
-import App from './Stores-Test-Container.svelte';
-
-const app = new App({
-  target: document.body
-});
-
-export default app;
