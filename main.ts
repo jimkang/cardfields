5302319a72bd3385d8edde7c18bf417420e99c1e
Jim Kang <jimkang@gmail.com>
2021-05-03 17:12:44 -0400

    Start working in main app.

--- main.ts
+++ main.ts
@@ -1,2 +1,26 @@
 var container = {};
 export default container;
+
+import { assembleMainMachine } from './machines/main-machine';
+import { StoreType, Thing } from './types';
+import { RenderPortControls } from './renderers/import-export-renderers';
+import {
+  exportFromLocalStorage,
+  importToLocalStorage,
+} from './persisters/import-export';
+
+assembleMainMachine(changePiles);
+
+var renderPortControls = RenderPortControls(
+  '.port-controls',
+  exportFromLocalStorage,
+  importToLocalStorage
+);
+renderPortControls();
+
+function changePiles(activeDeckIdentifier: StoreType<Thing>) {
+  // TODO: Tear down previous machine.
+  //var deckStore: ThingStoreType = registry.getStore(
+  //activeDeckIdentifier.get().deck
+  //);
+}
--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@
 import createConfig from './rollup-tools/base-config';
 import { serve } from './rollup-tools/config-tools';
 
-var vats = ['wily', 'decks', 'piles'];
+var vats = ['wily', 'piles'];
 // Inspired by https://github.com/Tom-Siegel/multi-page-svelte/blob/5dd47f9ffe3cbddbaa5e29be5056ce1ed56060b2/rollup-pages.config.js#L45
 var configs = [
   {
